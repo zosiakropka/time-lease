64e2571f24f9e58f630c4fb848ae7a9ebdb90991
Zosia <sobocinska.zosia@gmail.com>
2013-12-07 20:58:00 +0100

    @authenticate decorator

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -7,3 +7,18 @@ from timelease.settings import LOGIN_URL
 import logging
 
 
+class authenticate:
+    f = None
+    user = None
+
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, request, *args, **kwargs):
+        user = request.user
+        if not (user and user.is_authenticated()):
+            return redirect('%s/?next=%s' % (LOGIN_URL, request.path))
+        else:
+            return self.f(user, request, *args, **kwargs)
+
+
