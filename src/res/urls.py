53931b0853b090f9f589a5e817c150079699df05
Zosia <sobocinska.zosia@gmail.com>
2013-12-09 18:06:02 +0100

    resource_detail renamed to resource_calendar; fully rewritten template and view

--- src/res/urls.py
+++ src/res/urls.py
@@ -10,13 +10,13 @@ from django.utils import timezone
 urlpatterns = patterns('')
 urlpatterns += patterns('res.views',
     url(r'^$', 'resource_list'),
+
     url(r'^(?P<resource_slug>[a-zA-Z0-9_.-]+)?/$',
-        'resource_detail',
-        {"year": timezone.now().year, "month": timezone.now().month}),
-    url(r'^(?P<resource_slug>[a-zA-Z0-9_.-]+)/(?P<year>\d{4})-(?P<month>\d{2})?/$',
-        'resource_detail'),
-    url(r'^(?P<resource_slug>[a-zA-Z0-9_.-]+)/(?P<year>\d{4})-(?P<month>\d{2})?/(prev|next)?/$',
-        'resource_detail', {}),
-    url(r'^(?P<resource_slug>[a-zA-Z0-9_.-]+)/(prv|nxt)?/$',
-        'resource_detail', {}),
+        'resource_calendar',
+        {"year": timezone.now().year, "month": timezone.now().month},
+        name="resource_calendar"),
+
+    url(r'^(?P<resource_slug>[a-zA-Z0-9_.-]+)/(?P<year>\d{4})-(?P<month>\d{1,2})?/$',
+        'resource_calendar',
+        name="resource_calendar"),
 )
