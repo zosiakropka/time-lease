e4647b654d3e09f9da01f0924c13ca7734371b63
Zosia <sobocinska.zosia@gmail.com>
2013-12-09 18:01:41 +0100

    Resource Occupation class fully redefined

--- src/res/models.py
+++ src/res/models.py
@@ -6,7 +6,9 @@
 from django.db import models
 from recurrence.fields import RecurrenceField
 from django.core.validators import MaxValueValidator
-from django.db.models.fields import IntegerField
+from django.db.models.fields import IntegerField, CharField
+from django.db.models.fields.related import ForeignKey
+from django.contrib.auth.models import User
 
 
 class Resource(models.Model):
@@ -21,29 +23,44 @@ class Resource(models.Model):
         return self.name
 
 
-class DayPoint(models.Model):
-    """
-    """
-    hour = models.IntegerField(validators=[MaxValueValidator(23)])
-    quarter = models.IntegerField(validators=[MaxValueValidator(3)])
-
-
 class Occupation(models.Model):
     """
     """
-    daypoint = DayPoint()
+    QUARTERS = 4
+    HOURS = 24
+    
+    BOOK = 'b'
+    LIKE = 'l'
+    LEVELS = (
+        (LIKE, "Like"),
+        (BOOK, "Book")
+    )
+    user = models.ForeignKey(User, related_name="occupations")
+    hour = models.IntegerField(validators=[MaxValueValidator(23)])
+    quarter = models.IntegerField(validators=[MaxValueValidator(3)])
+    resource = ForeignKey(Resource, related_name="occupations")
     quarters = IntegerField()
-
-
-class SingleOccupation(Occupation):
-    """
-    """
-    date = models.TimeField()
-
-
-class MultiOccupation(Occupation):
-    """
-    """
-    start = models.TimeField()
-    end = models.TimeField()
     recurrences = RecurrenceField()
+    level = CharField(max_length=1, choices=LEVELS)
+
+    @property
+    def abs_quarter_start(self):
+        return 4 * self.hour + self.quarter
+
+    @property
+    def abs_quarter_stop(self):
+        return self.abs_quarter_start + self.quarters
+
+    @staticmethod
+    def abs_quarter(hour, quarter):
+        return 4 * hour + quarter
+
+    def __unicode__(self):
+        return self.user.username
+
+    def date_overlaps(self, date):
+        return date in self.recurrences.rdates
+
+    def quarter_overlaps(self, hour, quarter):
+        abs_quarter = self.abs_quarter(hour, quarter)
+        return self.abs_quarter_start < abs_quarter and self.abs_quarter_stop > abs_quarter
